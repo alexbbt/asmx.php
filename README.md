c8d986ab113c33fce4f4d0c016780f5d9b695572
alexbbt <alexander@bell-towne.com>
2016-05-17 22:52:13 -0700

    upload

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # asmx.php
 Redirect local asmx calls through php to a web asmx, used for frontend development of Azure Sites
+Replace all calls to "api.asmx/" with "api.php?"
+Update $asmxURL with the link to your ASMX file
+Place in same folder as "api.asmx/"
+Run a local PHP server in the root folder
+It will handle the serving of all HTML, CSS, and JS
+Bash Command: 
+``` Bash
+php -S 127.0.0.1:8080
+```
---
+++ api.php
@@ -0,0 +1,22 @@
+<?php
+
+	/*
+	 * Replace all calls to "api.asmx/" with "api.php?"
+	 * Update $asmxURL with the link to your ASMX file
+	 * Place in same folder as "api.asmx/"
+	 * Run a local PHP server in the root folder
+	 * It will handle the serving of all HTML, CSS, and JS
+	 * Unix Command: 'php -S 127.0.0.1:8080'
+	 */
+
+	$asmxURL = "http://webcrawleralexbbt.cloudapp.net/api.asmx/";
+
+	$url = $asmxURL . array_keys($_GET)[0];
+
+	if (strlen($_GET[array_keys($_GET)[0]]) > 0) {
+		$url .= "=" . $_GET[array_keys($_GET)[0]];
+	}
+
+	echo file_get_contents($url);
+
+?>
